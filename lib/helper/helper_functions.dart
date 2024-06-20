ebe9b06089c4e360368408c348fb646b5fa76aaa
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-06-21 01:09:53 +0300

    ,

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 // display error message to user
 void displayMessageToUser(String message, BuildContext context) {
   showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-            title: Text(message),
-          ));
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(message),
+    ),
+  );
 }
